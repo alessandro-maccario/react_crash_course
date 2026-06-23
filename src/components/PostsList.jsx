1529f9e876d8f41a487b09932d85284a7c95b872
alessandro.maccario <alessandro.maccario@proton.me>
2026-06-23 06:47:53 +0200

    feat: add newpost and newpost module css

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,17 @@
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
 import Post from "./Post";
 
 export default function PostList(props) {
   return (
-    <ul className={classes.posts}>
-      <Post author="Mars" body="React is awesome!" />
-      <Post author="Marble" body="React is still awesome!" />
-    </ul>
+    // fragment to wrap the siblings together because React wants only one Root element returned
+    <>
+      <NewPost />
+
+      <ul className={classes.posts}>
+        <Post author="Mars" body="React is awesome!" />
+        <Post author="Marble" body="React is still awesome!" />
+      </ul>
+    </>
   );
 }
