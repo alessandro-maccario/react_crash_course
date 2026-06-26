fc756d15c4d0e8a0011c696008e4942f31dffa22
alessandro.maccario <alessandro.maccario@proton.me>
2026-06-26 09:04:23 +0200

    feat: add usestate lists and functions to control the newpost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,16 +1,32 @@
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Post from "./Post";
+import { useState } from "react";
 
-export default function PostList(props) {
+export default function PostList() {
+  const [enteredBody, setEnteredBody] = useState("Lorem Ipsum");
+  const [enteredAuthor, setEnteredAuthor] = useState("Max Mustermann");
+
+  // this function gets called whenever there is a change in the textarea
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuhorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     // fragment to wrap the siblings together because React wants only one Root element returned
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuhorHandler}
+      />
 
       <ul className={classes.posts}>
-        <Post author="Mars" body="React is awesome!" />
-        <Post author="Marble" body="React is still awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Maple" body="React ist verdammt geil!" />
       </ul>
     </>
   );
